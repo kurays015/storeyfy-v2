a141de076d5eb9e1ba6c4f3c79dc0c855df74879
Christzxz <christnarvarte15@gmail.com>
2024-06-14 22:58:14 +0800

    remove fixed style of signin signup page

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -2,7 +2,7 @@ import SignInForm from "./sign-in-form";
 
 export default function LoginPage() {
   return (
-    <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-[40%]">
+    <div className="flex justify-center my-12">
       <div className="px-16 py-6 rounded-2xl border border-gray-300">
         <h1 className="text-center text-2xl font-semibold mb-8">
           Welcome to Storeyfy!
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,7 +2,7 @@ import SignUpForm from "@/app/signup/sign-up-form";
 
 export default function SignUpPage() {
   return (
-    <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-[40%]">
+    <div className="flex justify-center my-12">
       <div className="px-16 py-6 rounded-2xl border border-gray-300">
         <h1 className="text-center text-2xl font-semibold mb-8">
           Sign up now!
