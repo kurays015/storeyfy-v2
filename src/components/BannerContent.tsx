06af827017ecf0d5b68fa1ed367ab999e8d2afc2
Christzxz <christnarvarte15@gmail.com>
2024-06-15 23:40:06 +0800

    add hehe

--- src/components/BannerContent.tsx
+++ src/components/BannerContent.tsx
@@ -8,7 +8,9 @@ export default function BannerContent({
 }: BannerContentProps) {
   return (
     <div className="absolute top-1/2 left-1/4 -translate-x-1/2 -translate-y-1/2 max-w-[450px]">
-      <h2 className="text-pink-300 text-3xl tracking-wide mb-4">{headline}</h2>
+      <h2 className="text-pink-300 text-3xl tracking-wide mb-4">
+        {headline}hehehe
+      </h2>
       <p className="font-bold text-5xl uppercase">{description}</p>
       <p className="my-4 text-2xl text-gray-500">
         starting at <span className="text-3xl font-bold">${price}</span>
