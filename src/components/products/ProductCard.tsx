25ecb3303237412152d9266cf30cf139b8f72b85
Christzxz <christnarvarte15@gmail.com>
2024-06-15 23:06:58 +0800

    add link in product cards

--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 import Rating from "../Rating";
 import Price from "./Price";
 import { ProductProps } from "@/types";
+import Link from "next/link";
 
 export default function ProductCard({
+  id,
   image,
   title,
   category,
@@ -12,18 +14,20 @@ export default function ProductCard({
   discount,
 }: ProductProps) {
   return (
-    <div className="flex flex-col justify-evenly gap-2 rounded-xl border overflow-hidden p-4">
-      <Image
-        src={image}
-        height={500}
-        width={500}
-        alt={title}
-        className="dark:text-white text-black"
-      />
-      <p className="text-red-400 uppercase text-sm font-medium">{category}</p>
-      <h3 className="text-slate-500 text-lg">{title}</h3>
-      <Rating rating={rating} />
-      <Price price={price} discount={discount} />
-    </div>
+    <Link href={`/product/${title}/${category}/${id}`}>
+      <div className="flex flex-col justify-evenly gap-2 rounded-xl border overflow-hidden p-4">
+        <Image
+          src={image}
+          height={500}
+          width={500}
+          alt={title}
+          className="dark:text-white text-black"
+        />
+        <p className="text-red-400 uppercase text-sm font-medium">{category}</p>
+        <h3 className="text-slate-500 text-lg">{title}</h3>
+        <Rating rating={rating} />
+        <Price price={price} discount={discount} />
+      </div>
+    </Link>
   );
 }
--- src/components/products/ProductMiniCard.tsx
+++ src/components/products/ProductMiniCard.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { ProductMiniCardProps } from "@/types";
 import Rating from "@/components/Rating";
 import Price from "@/components/products/Price";
+import Link from "next/link";
 
 export default function ProductMiniCard({
   id,
@@ -15,26 +16,28 @@ export default function ProductMiniCard({
   hasBorder,
 }: ProductMiniCardProps) {
   return (
-    <div
-      className={`flex w-full gap-2 overflow-hidden ${
-        hasBorder && "border border-gray-300 rounded-xl"
-      }`}
-    >
-      <Image
-        src={image}
-        width={100}
-        height={100}
-        alt={title}
-        style={{ width: 100, height: 100 }}
-      />
-      <div className="self-center">
-        <h1 className="text-sm text-ellipsis overflow-hidden whitespace-nowrap w-[160px] font-bold">
-          {title}
-        </h1>
-        <p className="text-slate-500 text-sm">{category}</p>
-        <Rating rating={rating} />
-        <Price price={price} discount={discount} />
+    <Link href={`/product/${title}/${category}/${id}`}>
+      <div
+        className={`flex w-full gap-2 overflow-hidden ${
+          hasBorder && "border border-gray-300 rounded-xl"
+        }`}
+      >
+        <Image
+          src={image}
+          width={80}
+          height={80}
+          alt={title}
+          style={{ width: 80, height: 80 }}
+        />
+        <div className="self-center">
+          <h1 className="text-sm text-ellipsis overflow-hidden whitespace-nowrap w-[160px] font-bold">
+            {title}
+          </h1>
+          <p className="text-slate-500 text-sm">{category}</p>
+          <Rating rating={rating} />
+          <Price price={price} discount={discount} />
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
