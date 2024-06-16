4353de9424701a2df89b513bd93adda745341c40
Christzxz <christnarvarte15@gmail.com>
2024-06-16 10:30:08 +0800

    add single product page

--- src/app/product/[title]/[category]/[id]/page.tsx
+++ src/app/product/[title]/[category]/[id]/page.tsx
@@ -1,3 +1,14 @@
-export default function ProductPage() {
-  return <div>ProductPage</div>;
+import db from "@/lib/db";
+
+export default async function SingleProductPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const product = await db.product.findFirst({
+    where: {
+      id: params.id,
+    },
+  });
+  return <div>{JSON.stringify(product)}</div>;
 }
