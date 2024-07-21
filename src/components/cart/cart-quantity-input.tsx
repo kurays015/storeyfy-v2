13c269c84301a46e0a5776398fa027d840cd9e43
Christzxz <christnarvarte15@gmail.com>
2024-07-21 19:48:41 +0800

    remove comment on cart input file

--- src/components/cart/cart-quantity-input.tsx
+++ src/components/cart/cart-quantity-input.tsx
@@ -3,7 +3,6 @@
 import { Input } from "@/components/ui/input";
 import { useCartStore } from "@/stores/cart-store";
 import { CartQuantityInputProps } from "@/types";
-// import { ChangeEvent } from "react";
 
 export default function CartQuantityInput({
   id,
@@ -13,19 +12,6 @@ export default function CartQuantityInput({
   const cartItemQuantity = useCartStore((state) => state.quantities[id]);
   const updateQuantity = useCartStore((state) => state.updateQuantity);
   const currentQuantity = cartItemQuantity > stock ? stock : cartItemQuantity;
-
-  // const handleQuantityChange = (e: ChangeEvent<HTMLInputElement>) => {
-  //   const value = e.target.value;
-
-  //   // Check if the input value is a valid number
-  //   if (!/^\d*$/.test(value)) {
-  //     return;
-  //   }
-
-  //   const newQuantity = Number(value);
-
-  //   updateQuantity(id, newQuantity);
-  // };
 
   return (
     <Input
