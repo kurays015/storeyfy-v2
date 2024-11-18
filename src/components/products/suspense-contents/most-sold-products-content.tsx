3d5961c3b9d1512af29d87bec432fe88d1e7577c
Christzxz <christnarvarte15@gmail.com>
2024-11-18 09:47:48 +0800

    uncomment DL

--- src/components/products/suspense-contents/most-sold-products-content.tsx
+++ src/components/products/suspense-contents/most-sold-products-content.tsx
@@ -4,14 +4,14 @@ import NotFound from "@/components/not-found";
 import db from "@/lib/db";
 
 export default async function MostSoldProductsContent() {
-  // const mostSoldProducts = await DL.query.getMostSoldProducts();
+  const mostSoldProducts = await DL.query.getMostSoldProducts();
 
-  const mostSoldProducts = await db.product.findMany({
-    where: {
-      rating: 5,
-    },
-    take: 12,
-  });
+  // const mostSoldProducts = await db.product.findMany({
+  //   where: {
+  //     rating: 5,
+  //   },
+  //   take: 12,
+  // });
 
   // console.log(mostSoldProducts, "TEST!");
 
