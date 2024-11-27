a88cb30e54a5f6d35bb07bd6a7c1d788296aac6c
Christzxz <christnarvarte15@gmail.com>
2024-11-27 17:46:03 +0800

    hide mobile nav if no session found

--- src/components/mobile/mobile-bottom-nav.tsx
+++ src/components/mobile/mobile-bottom-nav.tsx
@@ -11,6 +11,8 @@ export default async function MobileBottomNav() {
   const session = await DL.mutations.getSession();
   const orderCount = await DL.query.getUserOrdersCount(session?.user.id);
 
+  if (!session) return;
+
   return (
     <div className="fixed bottom-0 left-1/2 z-10 flex w-full max-w-[500px] -translate-x-1/2 items-center border bg-white p-4 shadow-customBoxShadow dark:border-none customSm:justify-between 480px:justify-around 480px:rounded-t-lg lg:hidden">
       <MobileBurgerMenu />
