9db32f1d3e30c4a36c76a4ed3aa71d616914fec5
christ <christnarvarte15@gmail.com>
2025-01-15 14:46:06 +0800

    fix siteMetadata url

--- src/config/siteMetadata.ts
+++ src/config/siteMetadata.ts
@@ -3,7 +3,7 @@ export const siteMetadata = {
   description:
     "Storeyfy is an ecommerce platform made by Christ Narvarte. Has a CRUD features, you can create, read, update, and delete product as you like, you can also add them to cart...",
   creator: "Christ Narvarte",
-  url: "https://storeyfy.vercel.app",
+  url: "https://storeyfy-v2.vercel.app",
   locale: "en_US",
   // image: ""
 };
